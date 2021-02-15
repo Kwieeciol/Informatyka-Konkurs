e257eaa810c443165d17124baa1524f9ec63096b
Lukasz Kwiecinski <lukkwie456@gmail.com>
2021-02-15 07:29:59 +0000

    Finished komnaty in JS

--- komnaty/main.js
+++ komnaty/main.js
@@ -84,12 +84,78 @@ function get_adjacent_cells(grid, y, x) {
     return delete_duplicates(adjacent_x.concat(adjacent_y));
 }
 
-const grid = [
-    ["#", "#", "#", "#", "#"],
-    ["#", ".", ".", "#", "#"],
-    ["#", ".", "#", ".", "#"],
-    ["#", "#", "#", ".", "#"],
-    ["#", "#", "#", "#", "#"]
-]
+function get_surrounding_positions(grid, y, x) {
+    let positions = [[y + 1, x], [y, x + 1], [y, x -1], [y + 1, x]];
+    let correct = [];
 
-console.log(get_adjacent_cells(grid, 3, 2));
+    for ([y, x] of positions) {
+        if (y > 0 && x > 0 && y <= grid.length && x <= grid.length) {
+            correct.push([y, x]);
+        }
+    }
+    return correct
+}
+
+function include(grid, pos) {
+    let [y, x] = pos;
+    for ([_y, _x] of grid) {
+        if (y == _y && x == _x) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function get_chamber(grid, y, x) {
+    let positions = get_adjacent_cells(grid, y, x);
+
+    for ([y, x] of positions) {
+        let surroundings = get_adjacent_cells(grid, y, x);
+        for (s of surroundings) {
+            if (!include(positions, s)) {
+                positions.push(s);
+            }
+        }
+    }
+    return delete_duplicates(positions);
+}
+
+function get_chamber_count(grid, chamber) {
+    let count = 0;
+
+    for ([y, x] of chamber) {
+        let surroundings = get_surrounding_positions(grid, y, x);
+        for ([_y, _x] of surroundings) {
+            let value = get_cell(grid, _y, _x)
+            if (value == "#") {
+                count += 1
+            }
+        }
+    }
+    return count
+}
+
+function main() {
+    let grid = [];
+    let free_spaces = [];
+
+    let n = Number(prompt("> "));
+
+    for (let i = 0; i < n; i++) {
+        let row = prompt("> ").split("");
+        grid.push(row);
+    }
+
+    for (let i = 0; i < n; i++) {
+        let [y, x] = prompt("> ").split(" ");
+        free_spaces.push([Number(y), Number(x)]);
+    }
+
+    for ([y, x] of free_spaces) {
+        let chamber = get_chamber(grid, y, x);
+        let count = get_chamber_count(grid, chamber);
+        console.log(count);
+    }
+}
+
+main();
